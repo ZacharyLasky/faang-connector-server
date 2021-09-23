95296c152aef746d8715f84771540679e1f3ec51
Zachary Lasky <zachary.lasky@W-DH-ML-ZLASKY2.local>
2021-09-23 18:22:07 -0400

    scrapes google and logs data!

--- server/web_scrapers/jobs/google.js
+++ server/web_scrapers/jobs/google.js
@@ -1,27 +1,22 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  let jobsUrl =
-    'https://careers.google.com/jobs/results/?category=DATA_CENTER_OPERATIONS&category=DEVELOPER_RELATIONS&category=HARDWARE_ENGINEERING&category=INFORMATION_TECHNOLOGY&category=MANUFACTURING_SUPPLY_CHAIN&category=NETWORK_ENGINEERING&category=PRODUCT_MANAGEMENT&category=PROGRAM_MANAGEMENT&category=SOFTWARE_ENGINEERING&category=TECHNICAL_INFRASTRUCTURE_ENGINEERING&category=TECHNICAL_SOLUTIONS&category=TECHNICAL_WRITING&category=USER_EXPERIENCE&company=Fitbit&company=Google&company=Google%20Fiber&company=Loon&company=Verily%20Life%20Sciences&company=Waymo&company=Wing&company=X&company=YouTube&distance=50&employment_type=FULL_TIME&employment_type=PART_TIME&employment_type=TEMPORARY&jex=ENTRY_LEVEL';
-  // Initialize browser
+const launchWebScraper = async () => {
   let browser = await puppeteer.launch();
-  // Open a new browser page
   let page = await browser.newPage();
-  // Go to specified url and tell browser that the navigation is finished
-  // when there are no more than 2 network connections for at least half of a second
+  let jobsUrl =
+    'https://careers.google.com/jobs/results/?category=DATA_CENTER_OPERATIONS&category=DEVELOPER_RELATIONS&category=HARDWARE_ENGINEERING&category=INFORMATION_TECHNOLOGY&category=MANUFACTURING_SUPPLY_CHAIN&category=NETWORK_ENGINEERING&category=PRODUCT_MANAGEMENT&category=PROGRAM_MANAGEMENT&category=SOFTWARE_ENGINEERING&category=TECHNICAL_INFRASTRUCTURE_ENGINEERING&category=TECHNICAL_SOLUTIONS&category=TECHNICAL_WRITING&category=USER_EXPERIENCE&company=Google&distance=50&employment_type=FULL_TIME&employment_type=TEMPORARY&jex=ENTRY_LEVEL';
   await page.goto(jobsUrl, { waitUntil: 'networkidle2' });
-  // Evaluate browser page
+
   let data = await page.evaluate(() => {
-    let jobTitles = [];
-    let jobQualifications = [];
-    let jobs = [];
     const jobResultNodes = document.querySelector('#search-results');
 
+    let jobTitles = [];
     const jobTitleNodes = jobResultNodes.querySelectorAll('h2[itemprop="title"]');
     jobTitleNodes?.forEach((node) => {
       jobTitles.push(node.innerText);
     });
 
+    let jobQualifications = [];
     const jobQualificationNodes = jobResultNodes.querySelectorAll(
       'div[itemprop="qualifications"] > ul > li'
     );
@@ -29,16 +24,18 @@ const puppeteer = require('puppeteer');
       jobQualifications.push(node.innerText);
     });
 
-    let jobResultArray = [...jobResultNodes.children];
+    let jobResultsArray = [...jobResultNodes.children];
     let jobResultInnerText = [];
-    for (let i = 0; i < jobResultArray.length; i++) {
-      jobResultInnerText.push(jobResultArray[i].innerText);
+    for (let i = 0; i < jobResultsArray.length; i++) {
+      jobResultInnerText.push(jobResultsArray[i].innerText);
     }
+
+    let jobs = [];
     jobResultInnerText?.forEach((text, i) => {
       if (text.includes(jobTitles[i])) {
         jobs.push({
           jobTitle: jobTitles[i],
-          jobQualifications: jobQualifications.filter((qualification) =>
+          jobQualifications: [...new Set(jobQualifications)].filter((qualification) =>
             text.includes(qualification)
           )
         });
@@ -49,8 +46,9 @@ const puppeteer = require('puppeteer');
       jobs
     };
   });
-  console.log(data);
-  debugger;
-  // Close browser
+
+  console.dir(data, { depth: null });
   await browser.close();
-})();
+};
+
+launchWebScraper();
