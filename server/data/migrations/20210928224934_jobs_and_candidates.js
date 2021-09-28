7ed65486f26ddd2c17c65ca063cb2ba039227c58
Zachary Lasky <zachary.lasky@W-DH-ML-ZLASKY2.local>
2021-09-28 18:57:08 -0400

    fix mig

--- server/data/migrations/20210928224934_jobs_and_candidates.js
+++ server/data/migrations/20210928224934_jobs_and_candidates.js
@@ -1,15 +1,4 @@
 exports.up = (knex) =>
-  knex.schema.createTable('jobs', (tbl) => {
-    tbl.increments();
-    tbl.text('job_title').notNullable();
-    tbl.specificType('job_qualifications', 'text ARRAY').notNullable();
-    tbl.text('company').notNullable();
-    tbl.text('jobs_url').notNullable();
-  });
-
-exports.down = (knex) => knex.schema.dropTableIfExists('jobs');
-
-exports.up = (knex) =>
   knex.schema.createTable('candidates', (tbl) => {
     tbl.increments();
     tbl.text('candidate_name').notNullable();
