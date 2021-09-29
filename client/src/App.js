4478f02481681c49c8dbea15048396afbf9f3c41
Zachary Lasky <zachary.lasky@W-DH-ML-ZLASKY2.local>
2021-09-28 21:25:11 -0400

    server call

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios
+      .get('http://localhost:4000/jobs')
+      .then((res) => {
+        console.log({ res });
+      })
+      .catch((err) => {
+        console.log({ err });
+      });
+  }, []);
+
   return (
     <div className="App">
       <h1>FAANG Connector Client</h1>
--- server/app.js
+++ server/app.js
@@ -1,17 +1,47 @@
 import express from 'express';
 import db from './data/db';
+const cors = require('cors');
 import { launchGoogleWebScraper } from './web_scrapers/jobs/google';
 import { launchCandidateWebScraper } from './web_scrapers/candidates/signalHire';
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('FAANG Connector Server');
-});
+app.use(
+  cors({
+    origin: '*'
+  })
+);
 
 app.listen(port, () => {
   console.log(`FAANG Connector server listening on port:${port}`);
+});
+
+app.get('/', (req, res) => {
+  console.log('hit!');
+  res.send('FAANG Connector Server');
+});
+
+app.get('/jobs', (req, res) => {
+  db('jobs')
+    .select('*')
+    .then((jobData) => {
+      res.status(200).send(jobData);
+    })
+    .catch((err) => {
+      res.status(500).send('Could not get job data. Failed with the following error: ', err);
+    });
+});
+
+app.get('/candidates', (req, res) => {
+  db('candidates')
+    .select('*')
+    .then((candidateData) => {
+      res.status(200).send(candidateData);
+    })
+    .catch((err) => {
+      res.status(500).send('Could not get candidate data. Failed with the following error: ', err);
+    });
 });
 
 const launchWebScrapers = async () => {
@@ -28,18 +58,17 @@ const launchWebScrapers = async () => {
             .returning('*')
             .then((res) => console.log(`Inserted ${res.length} job records`))
             .catch((err) =>
-              console.log('Could not insert data. Failed with the following error: ', err)
+              console.log('Could not insert job data. Failed with the following error: ', err)
             );
         })
         .catch((err) =>
-          console.log('Could not insert data. Failed with the following error: ', err)
+          console.log('Could not insert job data. Failed with the following error: ', err)
         );
     })
-    .catch((err) => console.log('Could not get data. Failed with the following error: ', err));
+    .catch((err) => console.log('Could not get job data. Failed with the following error: ', err));
 
   Promise.all(candidates)
     .then((candidateData) => {
-      console.log({ candidateData });
       db('candidates')
         .del()
         .then((res) => {
@@ -48,14 +77,16 @@ const launchWebScrapers = async () => {
             .returning('*')
             .then((res) => console.log(`Inserted ${res.length} candidate records`))
             .catch((err) =>
-              console.log('Could not insert data. Failed with the following error: ', err)
+              console.log('Could not insert candidate data. Failed with the following error: ', err)
             );
         })
         .catch((err) =>
-          console.log('Could not insert data. Failed with the following error: ', err)
+          console.log('Could not insert candidate data. Failed with the following error: ', err)
         );
     })
-    .catch((err) => console.log('Could not get data. Failed with the following error: ', err));
+    .catch((err) =>
+      console.log('Could not get candidate data. Failed with the following error: ', err)
+    );
 };
 
 launchWebScrapers();
