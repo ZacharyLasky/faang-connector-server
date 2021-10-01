0d803ad97e3671a8be8cc8834298e81c33347303
Zachary Lasky <zachary.lasky@W-DH-ML-ZLASKY2.local>
2021-10-01 13:16:59 -0400

    add pg variable

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   staging: {
     client: 'pg',
-    connection: process.env.DB_URL,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations'
     }
