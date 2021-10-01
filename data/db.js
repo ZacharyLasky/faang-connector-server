4e32b0b3709d8ef90595416fcb69f0a0f830ddfc
Zachary Lasky <zachary.lasky@W-DH-ML-ZLASKY2.local>
2021-10-01 13:23:17 -0400

    env to dbenv

--- data/db.js
+++ data/db.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const knexfile = require('../knexfile');
 const dbEnv = process.env.DB_ENV || 'development';
-const configOptions = knexfile[env];
+const configOptions = knexfile[dbEnv];
 
 module.exports = knex(configOptions);
